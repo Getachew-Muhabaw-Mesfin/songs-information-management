e5879de9db54cb1bcbe0e0d62b44e57752ae1b29
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-02-15 12:20:31 +0300

    Add Pie chart for Songs in Genre

--- client/src/pages/Statistics.tsx
+++ client/src/pages/Statistics.tsx
@@ -4,9 +4,28 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../services/redux/store";
 import { GET_STATISTICS } from "../services/redux/types";
 import Spinner from "../components/ui/Spinner";
-import { Bar } from "react-chartjs-2";
+import { Bar, Pie } from "react-chartjs-2";
 import { CategoryScale, Chart } from "chart.js/auto";
 Chart.register(CategoryScale);
+
+interface BarData {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    backgroundColor: string[];
+    borderWidth: number;
+  }[];
+}
+
+interface PieData {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    backgroundColor: string[];
+  }[];
+}
 
 const Statistics = () => {
   const dispatch = useDispatch();
@@ -17,36 +36,53 @@ const Statistics = () => {
     dispatch({ type: GET_STATISTICS });
   }, [dispatch]);
 
-  console.log(songsInGenre);
-  const barData = {
+  const barData: BarData = {
     labels: ["Total Songs", "Total Artists", "Total Albums", "Total Genres"],
     datasets: [
       {
         label: "Statistics",
         data: [totalSongs, totalArtists, totalAlbums, totalGenres],
+        backgroundColor: ["#24CEC8", "#C7D710", "#2174C9", "#FA8937"],
+        borderWidth: 1,
       },
     ],
   };
 
+  const pieData: PieData | null = songsInGenre
+    ? {
+        labels: Object.keys(songsInGenre),
+        datasets: [
+          {
+            label: "Songs in Genre",
+            data: Object.values(songsInGenre),
+            backgroundColor: [
+              "#24CEC8",
+              "#C7D710",
+              "#2174C9",
+              "#FA8937",
+              "#FF6384",
+            ],
+          },
+        ],
+      }
+    : null;
+
   return (
     <div>
       <Heading>Statistics</Heading>
-      <div>
-        {totalSongs === undefined ? (
-          <Spinner />
-        ) : (
-          <div>
-            <div>
-              <h3>Total Songs: {totalSongs}</h3>
-              <h3>Total Artists: {totalArtists}</h3>
-              <h3>Total Albums: {totalAlbums}</h3>
-              <h3>Total Genres: {totalGenres}</h3>
-              <Bar data={barData} />
-            </div>
-          </div>
-        )}
+      <div className="row">
+        <div className="col-md-6">
+          {totalSongs === undefined ? <Spinner /> : <Bar data={barData} />}
+        </div>
+        <div className="col-md-6">
+          {pieData && (
+            <>
+              <Heading>Songs in Genre</Heading>
+              <Pie data={pieData} />
+            </>
+          )}
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
